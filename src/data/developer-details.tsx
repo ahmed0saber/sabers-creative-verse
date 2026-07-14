8d311139b8d4e8af50f116c3d2292d929647ff13
Ahmed Saber <ahmed0saber33@gmail.com>
2026-07-15 01:31:54 +0300

    Remove "Mid-Level" from title

--- src/data/developer-details.tsx
+++ src/data/developer-details.tsx
@@ -1,7 +1,8 @@
 const developerDetails = {
-    name: 'Ahmed Saber',
-    title: 'Mid-Level Front-End Developer',
-    description: 'T-shaped software developer specialized in frontend web development, driven by a passion for teaching, mentoring, and content creation, and actively engaged in open-source initiatives.',
-}
+  name: "Ahmed Saber",
+  title: "Front-End Developer & Instructor",
+  description:
+    "T-shaped software developer specialized in frontend web development, driven by a passion for teaching, mentoring, and content creation, and actively engaged in open-source initiatives.",
+};
 
 export default developerDetails;
