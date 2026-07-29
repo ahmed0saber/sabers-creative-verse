9fca18e994d5ac723ed7ed0551290f1eba921b96
Ahmed Saber <ahmed0saber33@gmail.com>
2026-07-30 02:21:16 +0300

    Add Quantic to education

--- src/data/education.tsx
+++ src/data/education.tsx
@@ -1,26 +1,36 @@
 const education = [
-    {
-        degree: "Bachelor of Applied Science",
-        field: "Physics",
-        institution: "Kafr El-Sheikh University",
-        location: "Kafr El-Sheikh, Egypt",
-        period: "2019 - 2023",
-        achievements: [
-            "Our graduation project focuses on the development of a laser-controlled alert circuit, and studying some of its applications, including security systems, and measuring the level of wheat stored in silos, and by adjusting the sensitivity of the device to detect the absence of natural light, it can be used to turn on the street lamps automatically when it becomes dark outside."
-        ]
-    },
-    {
-        degree: "Specialization Diploma",
-        field: "Software Engineering",
-        institution: "Coursera",
-        location: "Online",
-        period: "2024 - 2025",
-        achievements: [
-            "IBM's Applied Software Engineering Fundamentals - Specialization",
-            "The Hong Kong University of Science and Technology's Software Engineering - Specialization",
-            "IBM's Introduction to DevOps - Course"
-        ]
-    }
+  {
+    degree: "Master of Science",
+    field: "Software Engineering",
+    institution: "Quantic School of Business and Technology",
+    location: "Online",
+    period: "May 2026 - Sep 2027",
+    achievements: [
+      "Began the foundation coursework on 23 May 2026 as part of the admissions process. Following admission approval, submission of the required documents, academic credential verification, and completion of the enrolment requirements, I officially enrolled in the program on 20 July 2026.",
+    ],
+  },
+  {
+    degree: "Bachelor of Science",
+    field: "Physics",
+    institution: "Kafr El-Sheikh University",
+    location: "Kafr El-Sheikh, Egypt",
+    period: "2019 - 2023",
+    achievements: [
+      "Our graduation project focuses on the development of a laser-controlled alert circuit, and studying some of its applications, including security systems, and measuring the level of wheat stored in silos, and by adjusting the sensitivity of the device to detect the absence of natural light, it can be used to turn on the street lamps automatically when it becomes dark outside.",
+    ],
+  },
+  {
+    degree: "Specialization Diploma",
+    field: "Software Engineering",
+    institution: "Coursera",
+    location: "Online",
+    period: "2024 - 2025",
+    achievements: [
+      "IBM's Applied Software Engineering Fundamentals - Specialization",
+      "The Hong Kong University of Science and Technology's Software Engineering - Specialization",
+      "IBM's Introduction to DevOps - Course",
+    ],
+  },
 ];
 
 export default education;
